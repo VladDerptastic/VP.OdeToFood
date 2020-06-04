4130c7af0667cd2fa51cfd15265f8536b84b0db9
Vladimir Petrov <sheyki@abv.bg>
2020-06-04 15:30:47 +0200

    Implementation not finished - Adding an edit record page for a restaurant.

---
+++ src/VP.OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VP.OdeToFood.Data;
+using VP.OdeToFood.Definition;
+
+namespace VP.OdeToFood.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        #region Fields
+        private readonly IRestaurantData _restaurantData;
+        #endregion
+
+        #region Properties
+        public Restaurant Restaurant { get; set; }
+        #endregion
+
+        public EditModel(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+        public IActionResult OnGet(int restaurantId)
+        {
+            Restaurant = _restaurantData.GetRestaurantById(restaurantId);
+            if (Restaurant == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+    }
+}
