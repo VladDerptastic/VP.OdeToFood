86b73d01b955d27da0ef0e835d9a9d766ef9aa79
Vladimir Petrov <sheyki@abv.bg>
2020-06-12 19:22:18 +0200

    Introduced a DataDbContex class.

---
+++ src/VP.OdeToFood.Data/DataDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VP.OdeToFood.Definition;
+
+namespace VP.OdeToFood.Data
+{
+    public class DataDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
