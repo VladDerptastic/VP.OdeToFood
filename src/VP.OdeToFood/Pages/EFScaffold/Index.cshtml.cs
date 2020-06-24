340d8bdaa82f1876ea3bc8fe708a95e71d59d31d
Vladimir Petrov <sheyki@abv.bg>
2020-06-24 09:45:24 +0200

    Created an automatic EntityFramework scaffolded views/models into a EFScaffold fodler, for the experience and to see some differences/best practices between the two.

---
+++ src/VP.OdeToFood/Pages/EFScaffold/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using VP.OdeToFood.Data;
+using VP.OdeToFood.Definition;
+
+namespace VP.OdeToFood.Pages.EFScaffold
+{
+    public class IndexModel : PageModel
+    {
+        private readonly VP.OdeToFood.Data.OdeToFoodDbContext _context;
+
+        public IndexModel(VP.OdeToFood.Data.OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Restaurant> Restaurant { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Restaurant = await _context.Restaurants.ToListAsync();
+        }
+    }
+}
