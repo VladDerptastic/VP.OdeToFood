7f7321ae13e8dabc89d4b90a9aad856cfa643ccc
Vladimir Petrov <sheyki@abv.bg>
2020-06-24 09:33:53 +0200

    Added a RestourantCountViewComponent which gets the restaurant count, caches it (and refreshes it every 10 minutes) and returns this information to our View Count.cshtml. We're using said view in our _Layout page at the moment.

---
+++ src/VP.OdeToFood/ViewComponents/RestaurantCountViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using VP.OdeToFood.Data;
+
+namespace VP.OdeToFood.ViewComponents
+{
+    public class RestaurantCountViewComponent : ViewComponent
+    {
+        private readonly IRestaurantData restaurantData;
+        private DateTime lastCheck;
+        private int cachedCount;
+
+        public RestaurantCountViewComponent(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+            
+            //initialize once
+            lastCheck = DateTime.UtcNow;
+            cachedCount = restaurantData.GetRestaurantCount();
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            if (DateTime.UtcNow >= lastCheck.AddMinutes(10)) //refresh the cache every 10 minutes
+                cachedCount = restaurantData.GetRestaurantCount();
+
+            return View("Count", cachedCount);
+        }
+    }
+}
